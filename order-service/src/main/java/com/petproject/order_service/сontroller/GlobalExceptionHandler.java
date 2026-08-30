71f167e63a866d08a37e3abcafcf0236ba78fcf0
Nikita <mikhaylovnikita05@gmail.com>
2026-08-30 18:38:38 +0300

    fix: update configurations, fix Docker

--- order-service/src/main/java/com/petproject/order_service/сontroller/GlobalExceptionHandler.java
+++ order-service/src/main/java/com/petproject/order_service/сontroller/GlobalExceptionHandler.java
@@ -1,4 +1,53 @@
 package com.petproject.order_service.сontroller;
 
+import com.petproject.order_service.entity.dto.ApiError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiError> handleNotFound(IllegalArgumentException ex) {
+        ApiError error = ApiError.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(e ->
+                errors.put(e.getField(), e.getDefaultMessage())
+        );
+
+        ApiError error = ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("Ошибка валидации данных")
+                .validationErrors(errors)
+                .timestamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleAllUncaughtException(Exception ex) {
+        ApiError error = ApiError.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("Внутренняя ошибка сервера. Пожалуйста, обратитесь в поддержку.")
+                .timestamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
 }
--- order-service/src/main/java/com/petproject/order_service/сontroller/OrderController.java
+++ order-service/src/main/java/com/petproject/order_service/сontroller/OrderController.java
@@ -1,5 +1,31 @@
 package com.petproject.order_service.сontroller;
 
-public class OrdersController {
-    
+import com.petproject.order_service.entity.dto.OrderCreateRequest;
+import com.petproject.order_service.entity.dto.OrderResponse;
+import com.petproject.order_service.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Long createOrder(
+            @RequestHeader("x-user-id") Long userId,
+            @Valid @RequestBody OrderCreateRequest request) {
+
+        return orderService.createOrder(userId, request);
+    }
+
+    @GetMapping("/{id}")
+    public OrderResponse getOrder(@PathVariable Long id) {
+        return orderService.getOrderById(id);
+    }
 }
